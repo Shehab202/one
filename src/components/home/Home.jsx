37b36d8868fb97aa6a8db30af245e8f49203a1d0
SHEHABQOTEIT <shehabqoteit@gmail.com>
2025-01-27 02:09:08 +0200

    add

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,12 +3,10 @@ import png from "../../assets/31a6af58-0274-4626-8122-baa042d160fa.jpeg";
 import cv from "../../assets/Shehab Hussein ,cv.pdf";
 import Social from "./Social";
 import gsap from "gsap";
-
 import { useEffect, useRef } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
-
+import { motion } from "framer-motion";
 
 function Home() {
   const one = useRef(null);
@@ -24,7 +22,7 @@ function Home() {
         duration: 5,
         opacity: 1,
         ease: "power4.out",
-        delay:1,
+        delay: 1,
         scrollTrigger: {
           trigger: el,
         },
@@ -37,7 +35,7 @@ function Home() {
       {
         y: 0,
         duration: 5,
-        delay:1,
+        delay: 1,
         opacity: 1,
         ease: "power4.out",
         scrollTrigger: {
@@ -52,13 +50,30 @@ function Home() {
       <div className="container home-content" ref={one}>
         <h4>Hello I`m</h4>
         <h1>Sehab Hussein</h1>
-        <h4 className="text-light">A Frontend Developer</h4>
+        <div className="rr">
+          <motion.h4
+            className="text-light"
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ duration: 150, repeat: Infinity, delay:2 ,ease:"linear"}}
+          >
+            A Frontend Developer A Frontend Developer A Frontend Developer A
+            Frontend Developer A Frontend Developer A Frontend Developer A
+            Frontend Developer A Frontend Developer A Frontend Developer A
+            Frontend Developer A Frontend Developer A Frontend Developer A
+            Frontend Developer A Frontend Developer A Frontend Developer A
+            Frontend Developer A Frontend Developer Frontend Developer A Frontend Developer
+          </motion.h4>
+        </div>
         <div className="btns">
           <a className="btn" href={cv} download>
             Download CV
           </a>
 
-          <a className="btn btn-primary" href="">
+          <a
+            className="btn btn-primary"
+            href="https://api.whatsapp.com/send/?phone=201145082201&text&type=phone_number&app_absent=0"
+          >
             lets talk
           </a>
         </div>
